d86df2362d40882a9d9455bdb210d40f5172ca57
BlairABlake <k111ryousuke@gmail.com>
2023-10-04 08:09:12 +0900

    initial commit

---
+++ test/test.h
@@ -0,0 +1,15 @@
+//
+// Created by ryousuke kaga on 2023/10/03.
+//
+
+#ifndef MYALGORITHMS_TEST_H
+#define MYALGORITHMS_TEST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "munit/munit.h"
+#include "list.h"
+
+#define TEST(name) MunitResult name(const MunitParameter params[], void* user_data_or_fixture)
+
+#endif //MYALGORITHMS_TEST_H
